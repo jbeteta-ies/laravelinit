cd579608532eed4ad591e4982866d19cff6d6850
Javier Beteta <jbeteta@ieseljust.com>
2025-04-26 19:18:32 +0200

    Tema 4 Finalizadi

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'landing.index')->name('landing.index');
+Route::view('/about', 'landing.about')->name('landing.about');
+
+
