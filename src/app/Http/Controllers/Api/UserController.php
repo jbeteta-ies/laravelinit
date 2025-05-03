a7808eba9e4c8629763789a69c1fb9d9f59de585
Javier Beteta <jbeteta@ieseljust.com>
2025-05-03 19:54:44 +0200

    5.12 Finalizado

---
+++ src/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    function index(Request $request): JsonResponse
+    {
+        // You can use the User model to fetch users from the database
+        // For example:
+        // $users = User::all();
+        
+        // Return a JSON response with the users
+        {
+            return response()->json(UserResource::collection(
+                User::with('phone')->get()
+            ));
+        };
+    }   
+}
---
+++ src/routes/api.php
@@ -0,0 +1,8 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\UserController;
+
+Route::get('/users', [UserController::class, 'index'])->name('api.users.index');
+
